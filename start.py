609981a1ab800c60ebda22393729ad833b1d83da
FachryMuhammad <fachry_muhammad@hotmail.com>
2020-04-20 09:04:53 +0700

    a

--- start.py
+++ start.py
@@ -8,4 +8,23 @@ images,labels = mndata.load_training()
 t_images, t_labels = mndata.load_testing()
 
 
+#convert .m file ex1c_softmax.m
 
+#train.X = [ones(1,size(train.X,2)); train.X];     line 16 
+#test.X = [ones(1,size(test.X,2)); test.X];             17
+#train.y = train.y+1; % make labels 1-based.            18
+#test.y = test.y+1; % make labels 1-based.              19
+
+#line 16
+(x,y) = images.shape
+bias = np.ones((m,1))
+images = np.hstack((bias,images))
+
+#line 17
+(x2,y2) = t_images.shape
+t_bias = np.ones((m2,1))
+t_images = np.hstack((t_bias,t_images))
+
+#line 18
+
+#line 19
