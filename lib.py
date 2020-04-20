8b76641d57030ef03124d67dd83e0fa8a547f7f8
FachryMuhammad <fachry_muhammad@hotmail.com>
2020-04-20 20:29:22 +0700

    a

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-print_statistics(images, t_images, labels, t_labels):
+def print_statistics(images, t_images, labels, t_labels):
     #number of training dataset
     len(labels)
     #number of test dataset
@@ -13,3 +13,26 @@ print_statistics(images, t_images, labels, t_labels):
     #number of instances per class on test dataset
     for i in np.unique(t_labels):
         np.sum(t_labels==i)
+
+def add_thetas(images, theta):
+    sum = theta[0]
+    for i in range(1,len(images.iloc[0])):
+        sum += np.sum(theta[i+1]*images.iloc[:,i])
+    sum_exp_thetas= np.exp(sum)    
+    return sum_exp_thetas
+
+def hypothesis(theta, images, labels):
+    #(x,y) = images.shape
+    im_size = np.unique(labels)
+    t=theta
+    vec_theta=[]
+    sum_theta=[]
+
+    for i in range (im_size):
+        vec_theta.append(add_thetas(images, t[:,i]))
+        sum_theta = np.sum(vec_theta)
+
+    a=vec_theta
+    b=sum_theta
+    h = a/b
+    return h
