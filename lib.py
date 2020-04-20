99a9c7b81d568b8bc54198d72d6319e37d037dee
FachryMuhammad <fachry_muhammad@hotmail.com>
2020-04-20 09:04:58 +0700

    a

--- lib.py
+++ lib.py
@@ -14,15 +14,3 @@ print_statistics(images, t_images, labels, t_labels):
     for i in np.unique(t_labels):
         np.sum(t_labels==i)
 
-#From  .m file ex1c_softmax.m
-
-#train.X = [ones(1,size(train.X,2)); train.X]; 
-#test.X = [ones(1,size(test.X,2)); test.X];
-
-
-(x,y) = images.shape
-bias = np.ones((m,1))
-images = np.hstack((bias,images))
-(x2,y2) = t_images.shape
-t_bias = np.ones((m2,1))
-t_images = np.hstack((t_bias,t_images))
