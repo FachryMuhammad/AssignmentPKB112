c8601e57b6a3ae82f244d87d32402d40929bdf61
FachryMuhammad <fachry_muhammad@hotmail.com>
2020-04-20 09:13:21 +0700

    a

--- start.py
+++ start.py
@@ -15,12 +15,18 @@ print_statistics(images, t_images, labels, t_labels)
 #test.X = [ones(1,size(test.X,2)); test.X];             17
 #train.y = train.y+1; % make labels 1-based.            18
 #test.y = test.y+1; % make labels 1-based.              19
+#theta = rand(n,num_classes-1)*0.001;                   32
 
 (x,y) = images.shape
 bias = np.ones((x,1))
 images = np.hstack((bias,images))
 
-
 (x2,y2) = t_images.shape
 t_bias = np.ones((x2,1))
 t_images = np.hstack((t_bias,t_images))
+
+n=np.size(images)
+num_classes= 10
+
+theta = np.random(n,numclasses-1)*0.001
+
